d0a696719d856a8e42f0418a261169ce8f3a5501
ngjamesng <just.james.ng@gmail.com>
2020-03-09 17:01:34 -0700

    add comments for memeReducer

--- src/memeReducer.js
+++ src/memeReducer.js
@@ -1,14 +1,22 @@
-import { createStore }  from 'redux';
-const INITIAL_STATE = {memes: []};
+import { createStore } from 'redux';
+const INITIAL_STATE = { memes: [] };
 
-function memeReducer(state=INITIAL_STATE, action) {
+function memeReducer(state = INITIAL_STATE, action) {
 
   switch (action.type) {
     case "ADD_MEME":
-      return {...state, memes: [...state.memes, action.payload]};
+      return {
+        //we must spread the state, to make a copy...
+        ...state,
+        // ... then overwrite the memes with a copy of memes + the new item 
+        memes: [...state.memes, action.payload]
+      };
     case "DELETE_MEME":
       let updatedMemes = state.memes.filter(meme => meme.id !== action.payload.id);
-      return {...state, memes: updatedMemes};
+      return {
+        ...state,
+        memes: updatedMemes
+      };
     default:
       return state;
   }
